cad1027e784cfb7a398d9cffadbbd9d94df5b01d
hazelyazicii <nazlikucukler@gmail.com>
2023-12-20 13:59:12 +0300

    YuzdelikHesaplama

---
+++ örnek çalışma 1/örnek çalışma 1/Program.cs
@@ -0,0 +1,16 @@
+﻿//YÜZDELİK HESAPLAMA
+    
+{
+    double sayi, yuzde, sonuc;
+    Console.Write("Sayıyı Giriniz :");
+    sayi = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Yüzdeyi Giriniz :");
+    yuzde = Convert.ToDouble(Console.ReadLine());
+    sonuc = sayi * yuzde / 100;
+    Console.WriteLine("Girilen sayının % {0}' i : {1}", yuzde, sonuc);
+    Console.ReadKey();
+
+
+
+
+}
---
+++ örnek çalışma 1/örnek çalışma 2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Faktöriyel Hesaplama
+
+int number = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 1; i <= number; i++)
+{
+    factorial *= i;
+
+
+
+}
+Console.WriteLine($"Faktöriyel: {factorial}");
